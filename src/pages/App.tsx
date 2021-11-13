234d6ba2680e22d45b0b299ac362cdf085d6d29e
zhongqi <zhongqi@didiglobal.com>
2021-11-13 14:56:12 +0800

    first commit

---
+++ src/pages/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Routes from "@routes/index";
+import { BrowserRouter as Router } from "react-router-dom";
+import { RecoilRoot } from "recoil";
+import Banner from '@components/Banner/Banner';
+
+const App = () => {
+  return (
+    <RecoilRoot>
+      <Router basename='/'>
+         <Banner />
+        <Routes />
+      </Router>
+    </RecoilRoot>
+  );
+};
+export default App;
+
